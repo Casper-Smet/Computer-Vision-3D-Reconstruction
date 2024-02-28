ede7dcbc86f9879c8c0d2ed0e3fda43a14f24102
Casper <casper.smet@gmail.com>
2024-02-28 11:11:27 +0100

    Assignment now loads voxels

--- assignment.py
+++ assignment.py
@@ -1,8 +1,24 @@
-import glm
 import random
 import numpy as np
+import cv2
+import glm
+
 
 block_size = 1.0
+
+
+def read_camera_properties(filename):
+    fs = cv2.FileStorage(filename, cv2.FILE_STORAGE_READ)
+
+    camera_properties = {
+        "camera_matrix": fs.getNode("CameraMatrix").mat(),
+        "distortion_coefficients": fs.getNode("DistortionCoeffs").mat(),
+        "r_matrix": fs.getNode("RMatrix").mat(),
+        "t_matrix": fs.getNode("TMatrix").mat(),
+    }
+
+    fs.release()
+    return camera_properties
 
 
 def generate_grid(width, depth):
@@ -11,41 +27,78 @@ def generate_grid(width, depth):
     data, colors = [], []
     for x in range(width):
         for z in range(depth):
-            data.append([x*block_size - width/2, -block_size, z*block_size - depth/2])
-            colors.append([1.0, 1.0, 1.0] if (x+z) % 2 == 0 else [0, 0, 0])
+            data.append(
+                [x * block_size - width / 2, -block_size, z * block_size - depth / 2]
+            )
+            colors.append([1.0, 1.0, 1.0] if (x + z) % 2 == 0 else [0, 0, 0])
     return data, colors
 
 
 def set_voxel_positions(width, height, depth):
-    # Generates random voxel locations
-    # TODO: You need to calculate proper voxel arrays instead of random ones.
-    data, colors = [], []
-    for x in range(width):
-        for y in range(height):
-            for z in range(depth):
-                if random.randint(0, 1000) < 5:
-                    data.append([x*block_size - width/2, y*block_size, z*block_size - depth/2])
-                    colors.append([x / width, z / depth, y / height])
-    return data, colors
+    voxel_data = np.load(r"..\data\vox_data.npz")
+    true_vox, _stepsize, z_stepsize = (
+        voxel_data["vox_coords"],
+        voxel_data["_stepsize"],
+        voxel_data["z_stepsize"],
+    )
+
+    colors = np.full(true_vox.shape, (255, 0, 0))
+    return true_vox, colors
+
+
+def get_cam_pos(path):
+    # https://stackoverflow.com/a/14693971
+
+    voxel_data = np.load(r"..\data\vox_data.npz")
+    _stepsize, z_stepsize = (
+        voxel_data["_stepsize"],
+        voxel_data["z_stepsize"],
+    )
+
+    cam_props = read_camera_properties(path)
+    tvec = cam_props["t_matrix"]
+    rvec = cam_props["r_matrix"]
+    rmatrix = cv2.Rodrigues(rvec)[0]
+
+    pos = (-(rmatrix.T).dot(tvec) + 2000)
+    pos[0] = pos[0] / _stepsize  # TODO Z scales differently fix please
+    pos[1] = pos[1] / _stepsize  # TODO Z scales differently fix please
+    pos[2] = pos[2] / z_stepsize  # TODO Z scales differently fix please
+    return pos.reshape(3), rvec.reshape(3)
 
 
 def get_cam_positions():
     # Generates dummy camera locations at the 4 corners of the room
     # TODO: You need to input the estimated locations of the 4 cameras in the world coordinates.
-    return [[-64 * block_size, 64 * block_size, 63 * block_size],
-            [63 * block_size, 64 * block_size, 63 * block_size],
-            [63 * block_size, 64 * block_size, -64 * block_size],
-            [-64 * block_size, 64 * block_size, -64 * block_size]], \
-        [[1.0, 0, 0], [0, 1.0, 0], [0, 0, 1.0], [1.0, 1.0, 0]]
+
+    pos, rvec = zip(
+        *[get_cam_pos(f"../data/cam{i}/camera_properties.xml") for i in range(1, 5)]
+    )
+    return pos, [[1.0, 0, 0], [0, 1.0, 0], [0, 0, 1.0], [1.0, 1.0, 0]]
+    # return [[-64 * block_size, 64 * block_size, 63 * block_size],
+    #         [63 * block_size, 64 * block_size, 63 * block_size],
+    #         [63 * block_size, 64 * block_size, -64 * block_size],
+    #         [-64 * block_size, 64 * block_size, -64 * block_size]], \
+    #     [[1.0, 0, 0], [0, 1.0, 0], [0, 0, 1.0], [1.0, 1.0, 0]]
 
 
 def get_cam_rotation_matrices():
     # Generates dummy camera rotation matrices, looking down 45 degrees towards the center of the room
     # TODO: You need to input the estimated camera rotation matrices (4x4) of the 4 cameras in the world coordinates.
     cam_angles = [[0, 45, -45], [0, 135, -45], [0, 225, -45], [0, 315, -45]]
+    _, cam_angles = zip(
+        *[get_cam_pos(f"../data/cam{i}/camera_properties.xml") for i in range(1, 5)]
+    )
+    print(cam_angles)
     cam_rotations = [glm.mat4(1), glm.mat4(1), glm.mat4(1), glm.mat4(1)]
     for c in range(len(cam_rotations)):
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][0] * np.pi / 180, [1, 0, 0])
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][1] * np.pi / 180, [0, 1, 0])
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][2] * np.pi / 180, [0, 0, 1])
+        cam_rotations[c] = glm.rotate(
+            cam_rotations[c], cam_angles[c][0] * np.pi / 180, [1, 0, 0]
+        )
+        cam_rotations[c] = glm.rotate(
+            cam_rotations[c], cam_angles[c][1] * np.pi / 180, [0, 1, 0]
+        )
+        cam_rotations[c] = glm.rotate(
+            cam_rotations[c], cam_angles[c][2] * np.pi / 180, [0, 0, 1]
+        )
     return cam_rotations
